121418bcb679e2fdf0e921dee9b17bb7741e39f4
bishal <monster_bishal@live.com>
2017-07-10 14:55:18 +0545

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        modules: [__dirname, 'node_modules'],
+        extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+};
